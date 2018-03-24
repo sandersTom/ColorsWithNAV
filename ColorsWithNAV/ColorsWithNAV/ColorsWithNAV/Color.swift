4d0ac57279399fd089779314d8aed8648b59ae5a
sandersTom <31323914+sandersTom@users.noreply.github.com>
2018-03-23 19:12:00 -0500

    background
    added the colors to the background of each cell

---
+++ ColorsWithNAV/ColorsWithNAV/ColorsWithNAV/Color.swift
@@ -0,0 +1,16 @@
+//
+//  Color.swift
+//  ColorsWithNAV
+//
+//  Created by t s on 3/23/18.
+//  Copyright © 2018 Shuper. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    
+    let name: String
+    let uiColor: UIColor
+}
+
